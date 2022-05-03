5cd4f0a4e69b576f9b474598617d28a26007ff27
Victor Moreira <victor_farfam@hotmail.com>
2022-05-03 17:27:08 -0300

    Correção

--- bankline-api/bankline-api/src/main/java/com/dio/santander/bankline/api/model/Correntista.java
+++ bankline-api/bankline-api/src/main/java/com/dio/santander/bankline/api/model/Correntista.java
@@ -5,7 +5,13 @@ public class Correntista {
 	private String cpf;
 	private String nome;
 	
-	
+	private Conta conta;
+	public Conta getConta() {
+		return conta;
+	}
+	public void setConta(Conta conta) {
+		this.conta = conta;
+	}
 	public Integer getId() {
 		return id;
 	}
