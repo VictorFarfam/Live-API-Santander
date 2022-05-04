3118db7a300043f0a6407374cab13bfbc4bb712a
Victor Moreira <victor_farfam@hotmail.com>
2022-05-03 22:02:25 -0300

    Atualizacao pequena

--- bankline-api/bankline-api/src/main/java/com/dio/santander/bankline/api/model/Movimentacao.java
+++ bankline-api/bankline-api/src/main/java/com/dio/santander/bankline/api/model/Movimentacao.java
@@ -22,10 +22,13 @@ public class Movimentacao {
 	private LocalDateTime dataHora;
 	private String descricao;
 	private Double valor;
+
 	
 	@Enumerated(EnumType.STRING)
 	private MovimentacaoTipo tipo;
 	
+	@Column(name = "id_conta")
+	private Integer idConta;
 	public Integer getId() {
 		return id;
 	}
@@ -55,5 +58,11 @@ public class Movimentacao {
 	}
 	public void setTipo(MovimentacaoTipo tipo) {
 		this.tipo = tipo;
+	}
+	public Integer getIdConta() {
+		return idConta;
+	}
+	public void setIdConta(Integer idConta) {
+		this.idConta = idConta;
 	}
 }
